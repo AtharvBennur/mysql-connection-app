8186f448925b4d1beac777f0fcd5ed5763bccca2
AtharvBennur <atharvbennur@gmail.com>
2025-08-15 14:40:09 +0530

    chore: init node project, env example, ignore

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ const connection = mysql2.createConnection({
     database: process.env.DB_DATABASE
 });
 
+const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: 'mysql'
+});
 
 // Handle connection errors
 connection.on('error', (err) => {
